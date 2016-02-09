a95dd1928ceb55f318d292b9afd5f20fc64b61bc
Aurélien Matouillot <a.matouillot@gmail.com>
2016-02-09 11:53:40 +0100

    Add checker options

--- run.py
+++ run.py
@@ -146,8 +146,15 @@ class ThreadPoolThread(threading.Thread):
         self.__isDying = True
 
 
-def linkchecker_task(url):
-    process = subprocess.Popen(['./linkcheckerjs', url])
+def linkchecker_task(data):
+    cmd = ['./node_modules/.bin/phantomjs']
+    if data['ignore_ssl_errors'] is True:
+        cmd += ['--ignore-ssl-errors=yes']
+    cmd += ['lib/linkchecker.js']
+    if data['log_level'] is not None:
+        cmd += ['--log-level=%i' % data['log_level']]
+    cmd += [data['url']]
+    process = subprocess.Popen(cmd)
     process.communicate()
 
 
@@ -155,6 +162,11 @@ def main():
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="filename",
                       help="Urls list", metavar="FILE")
+    parser.add_option("-l", "--log-level", type="int", dest="log_level",
+                      help="linkcheckerjs log level")
+    parser.add_option("-i", "--ignore-ssl-errors", action="store_true",
+                      dest="ignore_ssl_errors",
+                      help="Ignore ssl errors for self signed certificate")
     (options, args) = parser.parse_args()
 
     if options.filename is None:
@@ -165,7 +177,10 @@ def main():
 
     with open(options.filename, 'r') as f:
         for url in f.readlines():
-            pool.queueTask(linkchecker_task, url)
+            pool.queueTask(linkchecker_task, {
+                'url': url.strip(),
+                'ignore_ssl_errors': options.ignore_ssl_errors,
+                'log_level':options.log_level})
 
     # When all tasks are finished, allow the threads to terminate
     pool.joinAll()
