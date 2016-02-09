8930028222aea482fc1b6c311065eb36e01862b3
Aurélien Matouillot <a.matouillot@gmail.com>
2016-02-09 23:59:56 +0100

    indent

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
 version = '0.0'
 
-setup(name='linkcheckerjs',
-      version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Aur\xc3\xa9lien Matouillot',
-      author_email='a.matouillot@gmail.com',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [console_scripts]
-      linkcheckerjs = linkcheckerjs.task:main
-      """,
-      )
+setup(
+    name='linkcheckerjs',
+    version=version,
+    description="",
+    long_description="""\
+    """,
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Aur\xc3\xa9lien Matouillot',
+    author_email='a.matouillot@gmail.com',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [console_scripts]
+    linkcheckerjs = linkcheckerjs.task:main
+    """,
+)
