9d86cd9a11d460783a129bb855119ddd702dad3a
Aurélien Matouillot <a.matouillot@gmail.com>
2016-03-31 18:45:33 +0200

    Same tests for requests and phantomjs checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -2,12 +2,12 @@
 
 import os
 import subprocess
-# import requests
 import time
+import copy
 
 from unittest import TestCase
 
-from linkcheckerjs.checker import phantomjs_checker
+from linkcheckerjs.checker import phantomjs_checker, requests_checker
 
 
 process = None
@@ -26,25 +26,26 @@ def teardown_module(module):
     process.kill()
 
 
-class TestJSLib(TestCase):
+class BaseTest(object):
+    checker_name = None
 
     def test_200_ok(self):
-        res = phantomjs_checker('http://localhost:8080')
+        url = 'http://localhost:8080'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': u'http://localhost:8080/',
             'redirect_url': None,
             'parent_url': None,
             'status': u'OK',
             'status_code': 200,
             'resources': [
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/css/style.css',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/',
                  'status': u'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/css/unexisting.css',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/',
@@ -55,12 +56,12 @@ class TestJSLib(TestCase):
                      'http://localhost:8080/page2.html',
                      'http://localhost:8080/unexisting']
         }]
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_ssl_error(self):
-        res = phantomjs_checker('https://localhost:8081')
+        url = 'https://localhost:8081'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': u'https://localhost:8081/',
             'redirect_url': None,
             'parent_url': None,
@@ -68,26 +69,25 @@ class TestJSLib(TestCase):
             'status_code': None,
             'resources': [],
             'urls': []}]
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_200_ok_ignore_ssl(self):
-        res = phantomjs_checker('https://localhost:8081',
-                                ignore_ssl_errors=True)
+        url = 'https://localhost:8081'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': u'https://localhost:8081/',
             'redirect_url': None,
             'parent_url': None,
             'status': u'OK',
             'status_code': 200,
             'resources': [
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'https://localhost:8081/static/css/style.css',
                  'redirect_url': None,
                  'parent_url': u'https://localhost:8081/',
                  'status': u'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'https://localhost:8081/static/css/unexisting.css',
                  'redirect_url': None,
                  'parent_url': u'https://localhost:8081/',
@@ -98,33 +98,33 @@ class TestJSLib(TestCase):
                      'https://localhost:8081/page2.html',
                      'https://localhost:8081/unexisting']
         }]
-        self.assertEqual(res, expected)
+        self.check(url, expected, ignore_ssl_errors=True)
 
     def test_301_redirect(self):
-        res = phantomjs_checker('http://localhost:8080/redirect-301')
+        url = 'http://localhost:8080/redirect-301'
         expected = [
             {
-                'checker': 'phantomjs',
+                'checker': self.checker_name,
                 'url': u'http://localhost:8080/redirect-301',
                 'redirect_url': u'http://localhost:8080',
                 'parent_url': None,
                 'status': u'Moved Permanently',
                 'status_code': 301},
             {
-                'checker': 'phantomjs',
+                'checker': self.checker_name,
                 'url': u'http://localhost:8080/',
                 'redirect_url': None,
                 'parent_url': u'http://localhost:8080/redirect-301',
                 'status': u'OK',
                 'status_code': 200,
                 'resources': [
-                    {'checker': 'phantomjs',
+                    {'checker': self.checker_name,
                      'parent_url': u'http://localhost:8080/',
                      'redirect_url': None,
                      'status': u'OK',
                      'status_code': 200,
                      'url': u'http://localhost:8080/static/css/style.css'},
-                    {'checker': 'phantomjs',
+                    {'checker': self.checker_name,
                      'parent_url': u'http://localhost:8080/',
                      'redirect_url': None,
                      'status': u'Not Found',
@@ -135,33 +135,33 @@ class TestJSLib(TestCase):
                          'http://localhost:8080/page2.html',
                          'http://localhost:8080/unexisting']}]
 
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_302_redirect(self):
-        res = phantomjs_checker('http://localhost:8080/redirect-302')
+        url = 'http://localhost:8080/redirect-302'
         expected = [
             {
-                'checker': 'phantomjs',
+                'checker': self.checker_name,
                 'url': u'http://localhost:8080/redirect-302',
                 'redirect_url': u'http://localhost:8080',
                 'parent_url': None,
                 'status': u'Moved Temporarily',
                 'status_code': 302},
             {
-                'checker': 'phantomjs',
+                'checker': self.checker_name,
                 'redirect_url': None,
                 'parent_url': u'http://localhost:8080/redirect-302',
                 'status': u'OK',
                 'status_code': 200,
                 'url': u'http://localhost:8080/',
                 'resources': [
-                    {'checker': 'phantomjs',
+                    {'checker': self.checker_name,
                      'parent_url': u'http://localhost:8080/',
                      'redirect_url': None,
                      'status': u'OK',
                      'status_code': 200,
                      'url': u'http://localhost:8080/static/css/style.css'},
-                    {'checker': 'phantomjs',
+                    {'checker': self.checker_name,
                      'parent_url': u'http://localhost:8080/',
                      'redirect_url': None,
                      'status': u'Not Found',
@@ -173,31 +173,31 @@ class TestJSLib(TestCase):
                     'http://localhost:8080/page2.html',
                     'http://localhost:8080/unexisting']}
         ]
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_page1(self):
-        res = phantomjs_checker('http://localhost:8080/page1.html')
+        url = 'http://localhost:8080/page1.html'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': u'http://localhost:8080/page1.html',
             'redirect_url': None,
             'parent_url': None,
             'status': u'OK',
             'status_code': 200,
             'resources': [
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/css/style.css',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page1.html',
                  'status': u'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/images/unexisting.png',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page1.html',
                  'status': u'Not Found',
                  'status_code': 404},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/images/image1.png',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page1.html',
@@ -205,83 +205,80 @@ class TestJSLib(TestCase):
                  'status_code': 200},
             ],
             u'urls': []}]
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_page2(self):
-        res = phantomjs_checker('http://localhost:8080/page2.html')
+        url = 'http://localhost:8080/page2.html'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': u'http://localhost:8080/page2.html',
             'redirect_url': None,
             'parent_url': None,
             'status': u'OK',
             'status_code': 200,
             'resources': [
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/redirect-static-301/css/style.css',
                  'redirect_url': u'http://localhost:8080/static/css/style.css',
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'Moved Permanently',
                  'status_code': 301},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/redirect-static-301/images/unexisting.png',
                  'redirect_url': u'http://localhost:8080/static/images/unexisting.png',
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'Moved Permanently',
                  'status_code': 301},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/redirect-static-302/images/image1.png',
                  'redirect_url': u'http://localhost:8080/static/images/image1.png',
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'Moved Temporarily',
                  'status_code': 302},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/images/unexisting.png',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'Not Found',
                  'status_code': 404},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/css/style.css',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/images/image1.png',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page2.html',
                  'status': u'OK',
                  'status_code': 200}],
             u'urls': []}]
-
-        res[0]['resources'].sort()
-        expected[0]['resources'].sort()
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_page3(self):
-        res = phantomjs_checker('http://localhost:8080/page3.html')
+        url = 'http://localhost:8080/page3.html'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             'url': 'http://localhost:8080/page3.html',
             'redirect_url': None,
             'parent_url': None,
             'status': 'OK',
             'status_code': 200,
             'resources': [
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/css/style.css',
                  'redirect_url': None,
                  'parent_url': 'http://localhost:8080/page3.html',
                  'status': u'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': u'http://localhost:8080/static/images/image1.png',
                  'redirect_url': None,
                  'parent_url': u'http://localhost:8080/page3.html',
                  'status': 'OK',
                  'status_code': 200},
-                {'checker': 'phantomjs',
+                {'checker': self.checker_name,
                  'url': 'http://localhost:8080/static/images/unexisting.png',
                  'redirect_url': None,
                  'parent_url': 'http://localhost:8080/page3.html',
@@ -290,12 +287,12 @@ class TestJSLib(TestCase):
             u'urls': []
         }]
 
-        self.assertEqual(res, expected)
+        self.check(url, expected)
 
     def test_unexisting(self):
-        res = phantomjs_checker('http://localhost:8080/unexisting.html')
+        url = 'http://localhost:8080/unexisting.html'
         expected = [{
-            'checker': 'phantomjs',
+            'checker': self.checker_name,
             "url": "http://localhost:8080/unexisting.html",
             "redirect_url": None,
             "parent_url": None,
@@ -304,4 +301,30 @@ class TestJSLib(TestCase):
             "resources": [],
             "urls": [],
         }]
+        self.check(url, expected)
+
+
+class TestPhantomjsChecker(TestCase, BaseTest):
+    checker_name = 'phantomjs'
+
+    def check(self, url, expected, ignore_ssl_errors=False):
+        res = phantomjs_checker(url, ignore_ssl_errors=ignore_ssl_errors)
+        for d in expected:
+            if 'resources' in d:
+                d['resources'].sort()
+        for d in res:
+            if 'resources' in d:
+                d['resources'].sort()
+        self.assertEqual(res, expected)
+
+
+class TestRequestsChecker(TestCase, BaseTest):
+    checker_name = 'requests'
+
+    def check(self, url, expected, ignore_ssl_errors=False):
+        expected = copy.deepcopy(expected)
+        for d in expected:
+            d.pop('resources', None)
+            d.pop('urls', None)
+        res = requests_checker(url, ignore_ssl_errors=ignore_ssl_errors)
         self.assertEqual(res, expected)
